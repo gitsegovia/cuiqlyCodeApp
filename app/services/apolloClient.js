13d27ec3b912adf401bca8829cd87383a1658b83
Jose Segovia <jose.segovia.r@gmail.com>
2021-07-15 21:17:06 -0500

    ios

--- app/services/apolloClient.js
+++ app/services/apolloClient.js
@@ -1,8 +1,8 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 const dev = process.env.NODE_ENV === `development`;
-// const URI_GRAPHQL = 'https://graph.cuiqly.com/';
-const URI_GRAPHQL = 'http://192.168.1.101:5000/';
+const URI_GRAPHQL = 'https://graph.cuiqly.com/';
+//const URI_GRAPHQL = 'http://192.168.1.101:5000/';
 
 const ClientApollo = new ApolloClient({
   uri: URI_GRAPHQL,
